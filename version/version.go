9068a65393de08392f8227f7339c1936594826b6
seymourtang <tangxianmeng@live.com>
2024-05-07 18:29:39 +0800

    feat(user-agent ):add custom user-agent

---
+++ version/version.go
@@ -0,0 +1,25 @@
+package version
+
+import (
+	"runtime"
+)
+
+// Version is the current version of the application
+
+const version = "0.0.1"
+
+func GetSDKVersion() string {
+	return version
+}
+
+func GetGoVersion() string {
+	return runtime.Version()
+}
+
+func GetOS() string {
+	return runtime.GOOS
+}
+
+func GetArch() string {
+	return runtime.GOARCH
+}
